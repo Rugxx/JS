ef095a1905c341ac1bfd868b6abaa6682f72fa07
Rūdolfs Petrovskis <rudolfs93@gmail.com>
2020-12-30 14:19:11 +0200

    ending of 16

---
+++ Assignments/Values and Vairables/script.js
@@ -0,0 +1,68 @@
+//              Assignment101 Values and Variables
+
+/*
+let country = "Latvia";
+let continent = "Europe";
+let population = 1957200;
+
+console.log('', country, '\n', continent, '\n', population);
+*/
+
+//              Assignment102 Data types
+
+/*
+let country = "Latvia";
+let continent = "Europe";
+let population = 1957200;
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+*/
+
+//              let, const and var
+
+/*
+const country = "Latvia";
+const continent = "Europe";
+let population = 1957200;
+
+let isIsland = true;
+const language = "latvian"
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+
+language = 'joke';
+
+*/
+
+//              Basic Operators
+
+const country = "Latvia";
+const continent = "Europe";
+let population = 1957200;
+const language = "latvian"
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6000000);
+console.log(33000000 > population);
+
+let description =
+    country +
+    ' is in ' +
+    continent +
+    ', and its ' +
+    population +
+    ' people speak ' +
+    language;
+
+console.log(description);
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,119 @@
+//          Vaules and Variables 01
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'jm';
+let $function = 23;
+
+let Person = "jonas"
+let PI = 3.1456;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+console.log(myFirstJob);
+*/
+
+//              Data types 02
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+//              let, const and var
+
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer'
+job = 'teacher'
+
+lastName = 'Schmedtmann';
+console.log(lastName);
+*/
+
+//              Basic Operators
+
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x +1 = 101
+x--; // x = x - 1 = 100
+x--;
+console.log(x);
+
+// Comparison operators
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+//              Operator Precedence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const avarageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, avarageAge);
