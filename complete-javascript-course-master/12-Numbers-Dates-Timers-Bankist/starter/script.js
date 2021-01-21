555540a91343e8ea558505e2c97bfbe729e0027a
Rūdolfs Petrovskis <rudolfs93@gmail.com>
2021-01-21 19:42:23 +0200

    BigINT

--- complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,147 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+//        Converting and checking numbers
+/*
+console.log(23 === 23.0);
+
+// Base 10 - 0 to 9
+// Bonary base 2 - 0 to 1
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+console.log(Number(`23`));
+console.log(+`23`); // Auto converts to numbers
+
+// Parsing (Need to start with a number)
+
+console.log(Number.parseInt(`30px`, 10));
+console.log(Number.parseInt(`e23`, 10));
+
+console.log(Number.parseFloat(` 2.5rem`)); // GO TO
+console.log(Number.parseInt(`  2.5rem`));
+
+console.log(parseFloat(`  2.5rem`));
+
+// Check if NaN (Not recommended)
+console.log(Number.isNaN(20));
+console.log(Number.isNaN(`20`));
+console.log(Number.isNaN(+`20X`));
+console.log(Number.isNaN(23 / 0)); // infinity no a NaN
+
+// Check if a value is number GO TO
+console.log(Number.isFinite(20));
+console.log(Number.isFinite(`20`));
+console.log(Number.isFinite(+`20X`));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(`23`));
+console.log(Number.isInteger(23 / 0));
+*/
+
+//        Math and Rounding
+/*
+console.log(Math.sqrt(36));
+console.log(36 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(1, 2, 3, 66, 123, 95, 22));
+console.log(Math.max(1, 2, 3, 66, `123`, 95, 22));
+console.log(Math.max(1, 2, 3, 66, `123x`, 95, 22));
+
+console.log(Math.min(1, 2, 3, 66, 123, 95, 22));
+
+console.log(Math.PI * Number.parseFloat(`10px`) ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0....1 -> 0...(max-min) -> min...max
+console.log(randomInt(10, 20));
+
+// Rounding Integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.6));
+console.log(Math.round(23.3));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(`23.9`));
+
+console.log(Math.floor(`23.3`));
+console.log(Math.floor(23.3));
+
+// Floor & trunk do the same for > 0 numbers
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3)); // rounding works the other way around
+
+// Floating point numbers (decimals)
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(4));
+*/
+
+//        Remainder Operator
+/*
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener(`click`, function () {
+  [...document.querySelectorAll(`.movements__row`)].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = `orangered`;
+    if (i % 3 === 0) row.style.backgroundColor = `blue`;
+  });
+});
+*/
+
+//        BigInt
+// Not saved after that
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 10);
+console.log(2 ** 53 + 100);
+console.log(2 ** 53 + 14);
+
+console.log(31231232131231231231245345n);
+console.log(BigInt(31231232131231231231245345n));
+
+// Operations
+
+// console.log(Math.sqrt(20n));
+
+console.log(10000n + 10000n);
+console.log(31231232135243242342n * 312312321312312n);
+
+const huge = 321312312093128290n;
+const num = 23;
+
+console.log(huge * BigInt(num));
+
+// Exception when num can be done with bigiNT
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(20n == 20);
+console.log(typeof 20n);
+
+console.log(huge + ` Is really big`);
+
+// Divisions
+console.log(10n / 3n); // floors, removes decimal part
+console.log(10 / 3);
